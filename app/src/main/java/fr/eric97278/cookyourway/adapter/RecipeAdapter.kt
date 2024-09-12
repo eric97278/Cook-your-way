cb4ab2247b3ee22b0cfdc85defa5ca94837e0977
eric97278 <159042489+eric97278@users.noreply.github.com>
2024-09-12 15:22:38 +0200

    ajout de recipeAdapter et de item_horizontal_recipe + ajout de la fonction pour injecter les images dans le recyclerview

---
+++ app/src/main/java/fr/eric97278/cookyourway/adapter/RecipeAdapter.kt
@@ -0,0 +1,31 @@
+package fr.eric97278.cookyourway.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.recyclerview.widget.RecyclerView
+import fr.eric97278.cookyourway.R
+
+class RecipeAdapter : RecyclerView.Adapter<RecipeAdapter.ViewHolder>(){
+
+    //boite pour ranger tout les composant a controller
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view){
+        val recipeImage = view.findViewById<ImageView>(R.id.image_item)
+
+    }
+        //Injecte le composant
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater
+            .from(parent.context)
+            .inflate(R.layout.item_horizontal_recipe, parent, false)
+            return ViewHolder(view)
+    }
+        //combien d'item dans la liste
+    override fun getItemCount(): Int = 5
+
+        //mets a jour chaque composant
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+
+    }
+}
--- app/src/main/java/fr/eric97278/cookyourway/fragments/HomeFragment.kt
+++ app/src/main/java/fr/eric97278/cookyourway/fragments/HomeFragment.kt
@@ -5,11 +5,21 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.RecyclerView
 import fr.eric97278.cookyourway.R
+import fr.eric97278.cookyourway.adapter.RecipeAdapter
 
 class HomeFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        val view = inflater.inflate(R.layout.fragment_home, container, false)
+
+        //recuperer le recyclerView
+        val horizontalRecyclerView = view.findViewById<RecyclerView>(R.id.horizontal_recycler_view)
+        horizontalRecyclerView.adapter = RecipeAdapter()
+
+        return view
+
+
     }
 }
