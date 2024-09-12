c53671401aaf4d893ff8207fa4ba97abb51d93ab
eric97278 <159042489+eric97278@users.noreply.github.com>
2024-09-12 15:02:55 +0200

    ajout du homefragment,fragment_home,images et icones dans drawable,implementation de string,colors, dimens et theme+ injection de homefragment dans le fragment_container

---
+++ app/src/main/java/fr/eric97278/cookyourway/MainActivity.kt
@@ -0,0 +1,27 @@
+package fr.eric97278.cookyourway
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import fr.eric97278.cookyourway.fragments.HomeFragment
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        // Injecter le fragment dans une boite (fragment_container)
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.fragment_container, HomeFragment())
+        transaction.addToBackStack(null)
+        transaction.commit()
+
+    }
+}
